19de63d4e77d48b14b33bbcbd82d9ff919c2d6b3
voralse <a.vorobyev@vpene.ru>
2022-05-30 23:03:45 +0500

    add css loading, add image loading

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import _ from "lodash";
+import "./style.css";
+import icon from "./icon.png";
 
 function component() {
 	const element = document.createElement("div");
 
 	// Lodash, now imported by this script
 	element.innerHTML = _.join(["Hello", "webpack"], " ");
+	element.classList.add("hello");
 
+	// Add the image to our existing div.
+	const myIcon = new Image();
+	myIcon.src = icon;
+
+	element.appendChild(myIcon);
+	
 	return element;
 }
 
